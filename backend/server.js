693aa7c33d774a289ca77f8832f55b566fc0b62d
oyewunmi gbenga kelvin <kelvinoye@gmail.com>
2024-12-23 09:14:38 +0100

    Setup mongodb for backend

---
+++ backend/server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGODB_URI);
+
+const todoSchema = new mongoose.Schema({
+  task: String,
+  completed: Boolean,
+});
+
+const Todo = mongoose.model('Todo', todoSchema);
+
+app.get('/todos', async (req, res) => {
+  const todos = await Todo.find();
+  res.json(todos);
+});
+
+app.post('/todos', async (req, res) => {
+  const todo = new Todo({
+    task: req.body.task,
+    completed: false,
+  });
+  await todo.save();
+  res.json(todo);
+});
+
+app.put('/todos/:id', async (req, res) => {
+  const todo = await Todo.findByIdAndUpdate(
+    req.params.id,
+    { completed: req.body.completed },
+    { new: true }
+  );
+  res.json(todo);
+});
+
+app.delete('/todos/:id', async (req, res) => {
+  await Todo.findByIdAndDelete(req.params.id);
+  res.json({ message: 'Todo deleted' });
+});
+
+app.listen(5000, () => console.log('Backend running on port 5000'));
