e62a5e8bf6ecd2d8c9c45cfd39083484e27c1808
Oyewunmi Gbenga <kelvinoye@gmail.com>
2025-03-16 19:42:42 +0100

    fix endpoint

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,13 +5,13 @@ export default function App() {
   const [task, setTask] = useState('');
 
   useEffect(() => {
-    fetch('http://localhost:5000/todos')
+    fetch('http://backend:5000/todos')
       .then((res) => res.json())
       .then((data) => setTodos(data));
   }, []);
 
   const addTodo = async () => {
-    const res = await fetch('http://localhost:5000/todos', {
+    const res = await fetch('http://backend:5000/todos', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ task }),
